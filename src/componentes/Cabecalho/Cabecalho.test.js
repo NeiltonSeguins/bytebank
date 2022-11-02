07a79dc97a1ac283991aed13b3616aadb7b340df
NeiltonSeguins <seguins.neilton@gmailcom>
2022-11-02 15:00:15 -0300

    Testando um componente

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,4 +1,8 @@
-test('Primeiro teste', () => {
-  const numero = 10;
-  expect(numero).toBe(11);
+import { render, screen } from '@testing-library/react';
+import Cabecalho from './index';
+
+test('Deve renderizar o nome do usuário logado', () => {
+  render(<Cabecalho />);
+  const nomeUsuario = screen.getByText('Joana Fonseca Dutra');
+  expect(nomeUsuario).toBeInTheDocument();
 });
