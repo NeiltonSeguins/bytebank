2a19fa3dc9a38da791c26ddf688c367eed7d0c23
NeiltonSeguins <seguins.neilton@gmailcom>
2022-11-09 15:03:51 -0300

    Testando uma função

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,3 +1,29 @@
+import { calculaNovoSaldo } from './index';
+
+describe('Quando eu realizo uma transação', () => {
+  test('Que é um depósito, o saldo deve aumentar', () => {
+    const transacao = {
+      transacao: 'Depósito',
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+
+    expect(novoSaldo).toBe(150);
+  });
+
+  test('Que é uma Transferência, o saldo deve ddiminuir', () => {
+    const transacao = {
+      transacao: 'Tranferência',
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+
+    expect(novoSaldo).toBe(50);
+  });
+});
+
 test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
   const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
 
