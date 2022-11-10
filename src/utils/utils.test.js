8042367f593cadc7610316243c3d2f4c9ab79f22
NeiltonSeguins <seguins.neilton@gmailcom>
2022-11-10 14:33:28 -0300

    Boas práticas em testes

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,7 +1,7 @@
 import { calculaNovoSaldo } from './index';
 
 describe('Quando eu realizo uma transação', () => {
-  test('Que é um depósito, o saldo deve aumentar', () => {
+  it('Que é um depósito, o saldo deve aumentar', () => {
     const transacao = {
       transacao: 'Depósito',
       valor: 50,
@@ -12,7 +12,7 @@ describe('Quando eu realizo uma transação', () => {
     expect(novoSaldo).toBe(150);
   });
 
-  test('Que é uma Transferência, o saldo deve ddiminuir', () => {
+  test('Que é uma Transferência, o saldo deve diminuir', () => {
     const transacao = {
       transacao: 'Tranferência',
       valor: 50,
@@ -24,7 +24,7 @@ describe('Quando eu realizo uma transação', () => {
   });
 });
 
-test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
+it('Deve retornar o valor do saldo atualizado com o rendimento', () => {
   const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
 
   const saldo = 100;
